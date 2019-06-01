933babe1df32fdd38d53880c6cbf997958d2d5b7
Tom Cooper <tomcooper@tec8.net>
2019-06-01 16:25:17 -0700

    working text footer

--- contact-sheet.py
+++ contact-sheet.py
@@ -1,14 +1,16 @@
 import PIL
-from PIL import Image,ImageDraw
+from PIL import Image,ImageDraw,ImageFont
 
+#orig image 800 × 450
+#contactsheet  - 1200x675 
+#contactsheet with footer - 1200x750 
+#75/3 = 25 pixels/footer
 R, G, B = 0, 1, 2
 
+fnt = ImageFont.truetype("readonly/fanwood-webfont.ttf",46)
 # read image and convert to RGB
 image=Image.open("readonly/msi_recruitment.gif")
 image=image.convert('RGB')
-height,width = image.size 
-#draw = ImageDraw.Draw(image)
-#draw.text((10,400),"testxxx")
 
 # build a list of 9 images which have different brightnesses
 #enhancer=ImageEnhance.Brightness(image)
@@ -24,18 +26,38 @@ for i in range(0, 9):
 
 # create a contact sheet from different brightnesses
 first_image=images[0]
-contact_sheet=PIL.Image.new(first_image.mode, (first_image.width*3,first_image.height*3))
+#print("width: " + str(first_image.width))
+#print("height: " + str(first_image.height))
+contact_sheet=PIL.Image.new(first_image.mode, (first_image.width*3,(first_image.height+50)*3))
+draw = ImageDraw.Draw(contact_sheet)
 x=0
 y=0
 
-for img in images:
+
+#x: 0 y: 0
+#x: 800 y: 0
+#x: 1600 y: 0
+#x: 0 y: 450
+#x: 800 y: 450
+#x: 1600 y: 450
+#x: 0 y: 900
+#x: 800 y: 900
+#x: 1600 y: 900
+for i in range(len(images)):
+    img = images[i]
+    #print("x: {} y: {}".format(x,y))
     # Lets paste the current image into the contact sheet
     contact_sheet.paste(img, (x, y) )
+    txt = "channel: {} intensity: {}".format(channels[i],intensities[i])
+    pixColor = img.getpixel((0,0))
+    print(pixColor)
+    draw.text((x,y+450),txt,fill=pixColor,font=fnt)
     # Now we update our X position. If it is going to be the width of the image, then we set it to 0
     # and update Y as well to point to the next "line" of the contact sheet.
+
     if x+first_image.width == contact_sheet.width:
         x=0
-        y=y+first_image.height
+        y=y+first_image.height + 50 
     else:
         x=x+first_image.width
 
